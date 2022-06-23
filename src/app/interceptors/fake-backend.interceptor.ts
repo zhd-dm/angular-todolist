fed22a3444421e176b5f912d1841904d1f21ea09
Demyan <dyo171717@gmail.com>
2022-06-23 11:52:02 +0300

    update: Обновлен fake-backend interceptor

--- src/app/interceptors/fake-backend.interceptor.ts
+++ src/app/interceptors/fake-backend.interceptor.ts
@@ -9,6 +9,7 @@ export class FakeBackendInterceptor implements HttpInterceptor {
   constructor() {}
 
   intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+
     if(request.url.includes('/api/auth')) {
       switch(request.method) {
         case 'GET':
@@ -17,6 +18,43 @@ export class FakeBackendInterceptor implements HttpInterceptor {
           return this.saveUser(request.body);
       }
     }
+
+    if(request.url.includes('api/tasks')) {
+      switch(request.method) {
+        case 'GET':
+          return this.getTasks();
+        case 'POST':
+          return this.saveTask(request.body);
+      }
+    }
+
+    if(request.url.includes(`api/tasks:${request.body.id}`)) {
+      switch(request.method) {
+        case 'PATCH':
+          return this.editTask(request.body);
+        case 'DELETE':
+          return this.deleteTask(request.body.id);
+      }
+    }
+
+    if(request.url.includes('api/categories')) {
+      switch(request.method) {
+        case 'GET':
+          return this.getCategories();
+        case 'POST':
+          return this.saveCategory(request.body);
+      }
+    }
+
+    if(request.url.includes(`api/categories:${request.body.id}`)) {
+      switch(request.method) {
+        case 'PATCH':
+          return this.editCategory(request.body);
+        case 'DELETE':
+          return this.deleteCategory(request.body.id);
+      }
+    }
+
     return next.handle(request);
   }
 
@@ -28,7 +66,7 @@ export class FakeBackendInterceptor implements HttpInterceptor {
 
   private saveUser(user: IUser): Observable<HttpEvent<IUser>> {
     return of(
-      new HttpResponse<IUser>({status: 200, body: {email: user.email, password: user.password}})
+      new HttpResponse<IUser>({status: 200, body: user})
     )
   }
 
@@ -38,9 +76,45 @@ export class FakeBackendInterceptor implements HttpInterceptor {
     )
   }
 
+  private saveTask(task: ITask): Observable<HttpEvent<ITask>> {
+    return of(
+      new HttpResponse<ITask>({status: 200, body: task})
+    )
+  }
+
+  private editTask(task: ITask): Observable<HttpEvent<ITask>> {
+    return of(
+      new HttpResponse<ITask>({status: 200, body: task})
+    )
+  }
+
+  private deleteTask(id: number): Observable<HttpEvent<number>> {
+    return of(
+      new HttpResponse<number>({status: 200})
+    )
+  }
+
   private getCategories(): Observable<HttpEvent<ICategory[]>> {
     return of(
       new HttpResponse<ICategory[]>({status: 200, body: []})
+    )
+  }
+
+  private saveCategory(category: ICategory): Observable<HttpEvent<ICategory>> {
+    return of(
+      new HttpResponse<ICategory>({status: 200, body: category})
+    )
+  }
+
+  private editCategory(category: ICategory): Observable<HttpEvent<ICategory>> {
+    return of(
+      new HttpResponse<ICategory>({status: 200, body: category})
+    )
+  }
+
+  private deleteCategory(id: number): Observable<HttpEvent<number>> {
+    return of(
+      new HttpResponse<number>({status: 200})
     )
   }
 
