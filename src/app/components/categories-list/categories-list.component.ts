45f9afd006caa9cfab7ea8c63df98f181e10fad3
Demyan <dyo171717@gmail.com>
2022-06-24 10:59:59 +0300

    feat: Добавлена сортировка категорий

--- src/app/components/categories-list/categories-list.component.ts
+++ src/app/components/categories-list/categories-list.component.ts
@@ -1,5 +1,7 @@
-import { Component, ViewChild } from '@angular/core';
+import { LiveAnnouncer } from '@angular/cdk/a11y';
+import { AfterViewInit, ChangeDetectionStrategy, ChangeDetectorRef, Component, ViewChild } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
+import { MatSort, Sort } from '@angular/material/sort';
 import { MatTable, MatTableDataSource } from '@angular/material/table';
 import { CategoryService } from 'src/app/services/category.service';
 import { ICategory } from 'src/types';
@@ -9,21 +11,37 @@ import { EditCategoryComponent } from '../edit-category/edit-category.component'
 @Component({
   selector: 'app-categories-list',
   templateUrl: './categories-list.component.html',
-  styleUrls: ['./categories-list.component.scss']
+  styleUrls: ['./categories-list.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class CategoriesListComponent {
+export class CategoriesListComponent implements AfterViewInit {
 
   categories: ICategory[] = this.categoryServise.getCategories();
-  table = new MatTableDataSource(this.categories);
-
-  constructor(
-    private dialogRef: MatDialog,
-    private categoryServise: CategoryService
-  ) { }
+  table: MatTableDataSource<ICategory> = new MatTableDataSource(this.categories);
 
   displayedColumns: string[] = ['id', 'name', 'settings'];
 
+  constructor(
+    private changeDetRef: ChangeDetectorRef,
+    private dialogRef: MatDialog,
+    private _liveAnnouncer: LiveAnnouncer,
+    private categoryServise: CategoryService
+  ) { }
+
   @ViewChild(MatTable) private categoriesTable: MatTable<ICategory> | undefined;
+  @ViewChild(MatSort) sort: MatSort = new MatSort;
+
+  ngAfterViewInit(): void {
+    this.table.sort = this.sort;
+  }
+
+  announceSortChange(sortState: Sort): void {
+    if(sortState.direction) {
+      this._liveAnnouncer.announce(`Sorted ${sortState.direction} ending`);
+    } else {
+      this._liveAnnouncer.announce(`Sorting cleared`);
+    }
+  }
 
   openModalEdit(row: ICategory): void {
     const modalEdit = this.dialogRef.open(EditCategoryComponent, { data: row });
